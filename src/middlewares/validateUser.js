0e88e83d625e2ca3161bce8f0ad0f1b75817f738
encarnacao <cencarnacaoq@gmail.com>
2023-01-21 23:32:11 -0300

    refactor: update validator to check if user exists

--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,10 +1,10 @@
+import { getDatabase } from "../database.js";
 import { signUpSchema, loginSchema } from "../schemas.js";
 
-
-function returnErrors(errors){
-    const { details } = errors;
+export function returnErrors(errors) {
+	const { details } = errors;
 	const message = details.map((i) => i.message).join(",");
-    return message;
+	return message;
 }
 
 export async function validateSignUp(req, res, next) {
@@ -21,14 +21,20 @@ export async function validateSignUp(req, res, next) {
 }
 
 export async function validateLogin(req, res, next) {
-    try {
-        const { error } = loginSchema.validate(req.body);
-        if (error) {
-            return res.status(422).send({ error: returnErrors(error) });
-        }
-        next();
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+	try {
+		const { error } = loginSchema.validate(req.body);
+		if (error) {
+			return res.status(422).send({ error: returnErrors(error) });
+		} else {
+			const user = await getDatabase()
+				.collection("users")
+				.findOne({ email });
+			if (!user)
+				return res.status(401).send("Email or password incorrect");
+		}
+		next();
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 }
