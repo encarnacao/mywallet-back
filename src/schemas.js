2539b722a34c01b0f9711bad667eb459a335912c
encarnacao <cencarnacaoq@gmail.com>
2023-01-22 00:07:59 -0300

    fix: validate dates properly

--- src/schemas.js
+++ src/schemas.js
@@ -1,4 +1,7 @@
-import Joi from 'joi';
+import JoiBase from 'joi';
+import JoiDate from "@joi/date";
+
+const Joi = JoiBase.extend(JoiDate);
 
 export const signUpSchema = Joi.object({
     name: Joi.string().required(),
@@ -14,5 +17,6 @@ export const loginSchema = Joi.object({
 export const entrySchema = Joi.object({
     description: Joi.string().required(),
     value: Joi.number().required(),
-    type: Joi.string().valid('expense', 'income').required()
+    type: Joi.string().valid('expense', 'income').required(),
+    date: Joi.date().format("DD/MM").required()
 });
