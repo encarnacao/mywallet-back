0acf82ad7ebfcf5baafe141d0e243ccc4900b96e
encarnacao <cencarnacaoq@gmail.com>
2023-01-23 14:00:54 -0300

    feat: add delete route to entries

--- src/controllers/entriesControllers.js
+++ src/controllers/entriesControllers.js
@@ -1,4 +1,5 @@
 import { getDatabase } from "../database.js";
+import { ObjectId } from "mongodb";
 
 export async function newEntry(req, res) {
 	try {
@@ -34,6 +35,31 @@ export async function getEntries(req, res){
         const response = {...user, entries};
         res.status(200).send(response);
     } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+
+export async function deleteEntry(req,res){
+    try{
+        const { authorization } = req.headers;
+        const token = authorization?.replace("Bearer ", "");
+        const session = await getDatabase().collection("sessions").findOne({token});
+        if(!session){
+            return res.status(401).send("Session expired");
+        }
+        const { id } = req.params;
+        const entry = await getDatabase().collection("entries").findOne({_id: ObjectId(id)});
+        if(!entry){
+            return res.status(404).send("Entry not found");
+        }
+        if(entry.userId.toString() !== session.userId.toString()){
+            return res.sendStatus(401);
+        }
+        await getDatabase().collection("entries").deleteOne({_id: ObjectId(id)});
+        res.sendStatus(200);
+    } catch(e){
         console.log(e);
         res.sendStatus(500);
     }
--- src/routes/entries.js
+++ src/routes/entries.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getEntries, newEntry } from "../controllers/entriesControllers.js";
+import { deleteEntry, getEntries, newEntry } from "../controllers/entriesControllers.js";
 import { validateEntry } from "../middlewares/validateEntries.js";
 
 const router = express.Router();
 
 router.post("/entries",validateEntry, newEntry);
 router.get("/entries", getEntries);
+router.delete("/entries/:id", deleteEntry);
 
 export default router;
